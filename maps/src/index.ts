a394f280bc5a2969958e21d44558f94c339bff68
Rahul Kulkarni <rahulkulkarni0510@gmail.com>
2020-03-19 21:35:52 -0500

    Design Patterns with TypeScript - Added Maps location example for User and Company - Classes and Interfaces interchange

---
+++ maps/src/index.ts
@@ -0,0 +1,10 @@
+import { CustomMap } from './CustomMap';
+import { User } from './User';
+import { Company } from './Company';
+
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap('map');
+
+customMap.addMarker(user);
+customMap.addMarker(company);
